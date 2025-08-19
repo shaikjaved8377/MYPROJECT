110604e5b23acd4a0fec20baad693c38223c141d
shaikjaved8377 <shaikjaved8377@gmail.com>
2025-08-18 23:27:00 -0500

    I used struct, arrays, slice

--- main.go
+++ main.go
@@ -1,9 +1,64 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
+type Person struct {
+	Name    string
+	Age     int
+	Address Address
+}
+
+type Address struct {
+	Street string
+	City   string
+	State  string
+	Zip    string
+}
+
+func main() {
+	p := Person{
+		Name:    "Javed",
+		Age:     22,
+		Address: Address{Street: "12355 devon St", City: "Chicago", State: "IL", Zip: "123"},
+	}
+	fmt.Println("Name:", p.Name)
+	fmt.Println("Age:", p.Age)
+	fmt.Println("Address:", p.Address.Street, p.Address.City, p.Address.State, p.Address.Zip)
+
+	//array example
+	a := [5]string{"Chicago", "Newyork", "Texas", "colorado", "Florida"}
+	for i, v := range a {
+		fmt.Println(i, v)
+	}
+
+	//slice we have 4 types-example
+	//type-1 -So len and cap are same value
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+	fmt.Println(s)
+	//type -2
+	s = make([]int, 3, 5)
+	s = append(s, 4, 5)
+	s = append(s, 6)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
+	// type -3
+	s = make([]int, 3)
+	s = append(s, 4)
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+	fmt.Println(s)
+
+	//type -4 slicing with an array
+	arr := []int{1, 2, 3, 4, 5}
+	s = arr[1:5]
+	fmt.Println(s)
+
+}
+
+/*
 // example for pointers
 func main() {
 	i := 6
@@ -55,7 +110,7 @@ func main() {
 		fmt.Println(a, "is odd")
 	}
 
-	//write for loo
+	//write for loop
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
@@ -72,3 +127,4 @@ func main() {
 		fmt.Println("Division by zero error")
 	}
 }
+*/
